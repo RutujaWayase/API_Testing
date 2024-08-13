363ee2ffd8605d5119980eabd33bf0914a6578de
RutujaWayase <rutujawayase1234@gmail.com>
2024-08-13 23:32:23 +0530

    adding authentication

--- RestAssuredProject/RestAssuredTraining/src/test/java/day7TypesOfAuthorizationsFakerLibrary/Authentications.java
+++ RestAssuredProject/RestAssuredTraining/src/test/java/day7TypesOfAuthorizationsFakerLibrary/Authentications.java
@@ -1,5 +1,25 @@
 package day7TypesOfAuthorizationsFakerLibrary;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
 public class Authentications {
+	
+	
+	@Test(priority=1)
+	void testBasicAuthentication() {
+		given()
+		  .auth().basic("postman", "password")
+		.when()
+		  .get("https://postman-echo.com/basic-auth")
+		.then()
+		  .statusCode(200)
+		  .body("authenticated", equalTo(true))
+		  .log().all();
+		
+	}
 
 }
