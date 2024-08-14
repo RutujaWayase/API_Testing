2b9990069d348bdd6540e4de64049afd6307144e
RutujaWayase <rutujawayase1234@gmail.com>
2024-08-14 09:03:36 +0530

    adding tests for authentication types of

--- RestAssuredProject/RestAssuredTraining/src/test/java/day7TypesOfAuthorizationsFakerLibrary/Authentications.java
+++ RestAssuredProject/RestAssuredTraining/src/test/java/day7TypesOfAuthorizationsFakerLibrary/Authentications.java
@@ -6,6 +6,23 @@ import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.Test;
 
+
+/*
+Authentication - valid or not
+Authorization - access
+
+Types of Authentication:
+	1) Basic
+    2) Digest
+    3) Preemptive
+    4) Bearer token
+    5) OAuth 1.0, 2.0
+    6) API Key
+
+*/
+
+
+
 public class Authentications {
 	
 	
@@ -18,9 +35,45 @@ public class Authentications {
 		.then()
 		  .statusCode(200)
 		  .body("authenticated", equalTo(true))
+		  .log().all();	
+	}
+	
+	@Test(priority=2)
+	void testDigestAuthentication() {
+		given()
+		  .auth().digest("postman", "password")
+		.when()
+		  .get("https://postman-echo.com/basic-auth")
+		.then()
+		  .statusCode(200)
+		  .body("authenticated", equalTo(true))
 		  .log().all();
+	}
+	//Preemptive Authentication: is a combination of Basic and Digest Authentication.
 
-		  
+	@Test(priority=3)
+	void testPreemptiveAuthentication() {
+		given()
+		  .auth().preemptive().basic("postman", "password")
+		.when()
+		  .get("https://postman-echo.com/basic-auth")
+		.then()
+		  .statusCode(200)
+		  .body("authenticated", equalTo(true))
+		  .log().all();
+	}
+	
+	@Test(priority=4)
+	void testBearerTokenAuthentication() {
+		String bearerToken="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";
+
+		given()
+		  .header("Authorization", "Bearer"+bearerToken)
+		.when()
+		  .get("https://api.github.com/user/repos")
+		.then()
+		  .statusCode(200)
+		  .log().all();
 		
 	}
 
