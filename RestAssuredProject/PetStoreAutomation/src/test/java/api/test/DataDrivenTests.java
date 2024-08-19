cd7971e9029e3e78c5e7a4ec6346c6c38c16d2c0
RutujaWayase <rutujawayase1234@gmail.com>
2024-08-19 23:54:57 +0530

    adding data driven tests

--- RestAssuredProject/PetStoreAutomation/src/test/java/api/test/DataDrivenTests.java
+++ RestAssuredProject/PetStoreAutomation/src/test/java/api/test/DataDrivenTests.java
@@ -1,5 +1,49 @@
 package api.test;
 
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import api.utilities.DataProviders;
+import io.restassured.response.Response;
+
 public class DataDrivenTests {
+	
+User userPayload;
+	
+	@Test(priority=1, dataProvider = "Data", dataProviderClass = DataProviders.class)
+	public void testPostUser(String userID, String userName, String fname, String lname, String mail, String pass, String ph){
+		
+		userPayload = new User();
+		
+		userPayload.setId(Integer.parseInt(userID));
+		userPayload.setUsername(userName);
+		userPayload.setFirstName(fname);
+		userPayload.setLastname(lname);
+		userPayload.setEmail(mail);
+		userPayload.setPassword(pass);
+		userPayload.setPhone(ph);
+	
+		Response res = UserEndPoints.createUser(userPayload);
+		Assert.assertEquals(res.getStatusCode(), 200);
+		
+	}
+	
+	@Test(priority = 2, dataProvider = "UserName", dataProviderClass = DataProviders.class)
+	public void testGetUserByName(String userName) {
+		
+		Response res = UserEndPoints.readUser(userName);
+		res.then().log().all();
+		
+		Assert.assertEquals(res.getStatusCode(), 200);
+	}
+	
+	@Test(priority=3, dataProvider = "UserName", dataProviderClass = DataProviders.class)
+	public void testDeleteUserByName(String userName) {
+		Response res = UserEndPoints.deleteUser(userName);
+		Assert.assertEquals(res.getStatusCode(), 200);
+	}
 
 }
--- RestAssuredProject/PetStoreAutomation/src/test/java/api/utilities/DataProviders.java
+++ RestAssuredProject/PetStoreAutomation/src/test/java/api/utilities/DataProviders.java
@@ -11,14 +11,14 @@ public class DataProviders {
 		String path = System.getProperty("user.dir")+"//testData//UserData.xlsx";
 		XLUtility xl = new XLUtility(path);
 		
-		int rownum = xl.getRowCount("Sheet2");
-		int colcount = xl.getCellCount("Sheet2", 1);
+		int rownum = xl.getRowCount("Sheet1");
+		int colcount = xl.getCellCount("Sheet1", 1);
 		
 		String apidata[][] = new String [rownum][colcount];
 		
 		for(int i = 1; i <= rownum; i++) {
 			for(int j = 0; j < colcount; j++) {
-				apidata[i-1][j] = xl.getCellData("Sheet2", i, j);
+				apidata[i-1][j] = xl.getCellData("Sheet1", i, j);
 			}
 			
 		}
@@ -33,14 +33,18 @@ public class DataProviders {
 		String path = System.getProperty("user.dir")+"//testData//UserData.xlsx";
 		XLUtility xl = new XLUtility(path);
 		
-		int rownum = xl.getRowCount("Sheet2");
+		int rownum = xl.getRowCount("Sheet1");
 		
 		String apidata[] = new String[rownum];
 		
 		for(int i = 1; i <= rownum; i++) {
-			apidata[i-1] = xl.getCellData("Sheet2", i, 1);
+			apidata[i-1] = xl.getCellData("Sheet1", i, 1);
 		}
 		return apidata;
 	}
+	
+	
+	
+	
 
 }
